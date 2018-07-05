6a5115284858f124fb7d81a60e6a2fd0a17539e8
aditishevale <aditi.shevale999@gmail.com>
2018-07-05 14:40:37 +0530

    Activities Entry in Manifest

---
+++ app/src/main/java/aditishevale/com/instagramclone/SearchActivity.java
@@ -0,0 +1,31 @@
+package aditishevale.com.instagramclone;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import aditishevale.com.instagramclone.Utils.BottomNavigationViewHelper;
+
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        setupBottomNavigationView();
+    }
+
+
+    private void setupBottomNavigationView(){
+        Log.d(TAG,"BottomNavigationView");
+
+        BottomNavigationViewEx bottomNavigationViewEx =(BottomNavigationViewEx)
+                findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+
+    }
+
+}
