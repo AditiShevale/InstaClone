f7d744a163a7f2ab7628d98fd9f5d6b31fc4e7d4
aditishevale <aditi.shevale999@gmail.com>
2018-07-05 11:59:07 +0530

    Image Assets

--- app/src/main/java/aditishevale/com/instagramclone/HomeActivity.java
+++ app/src/main/java/aditishevale/com/instagramclone/HomeActivity.java
@@ -4,12 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG,"onCreate:Starting");
 
     }
+    //Method For Stting BottomNavigation
+    private void setupBottomNavigationView(){
+        Log.d(TAG,"BottomNavigationView");
+
+        BottomNavigationViewEx bottomNavigationViewEx =(BottomNavigationViewEx)
+                findViewById(R.id.bottomNavViewBar);
+
+    }
+
 }
