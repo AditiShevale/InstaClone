f0b86f1b3308dd8674e9cb62f0fecc2706615d40
aditishevale <aditi.shevale999@gmail.com>
2018-07-07 11:34:12 +0530

    FragmentAdded

--- app/src/main/java/aditishevale/com/instagramclone/Home/CameraFragment.java
+++ app/src/main/java/aditishevale/com/instagramclone/Home/CameraFragment.java
@@ -17,6 +17,7 @@ public class CameraFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
        View view = inflater.inflate(R.layout.fragment_camera,container,false);
 
         return view;
