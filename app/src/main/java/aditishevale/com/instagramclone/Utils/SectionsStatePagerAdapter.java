2c56961d0495f8b7b5912d06dd85fb9183b3e934
aditishevale <aditi.shevale999@gmail.com>
2018-07-11 12:24:15 +0530

    class SectionsStatePagerAdapter Updated

--- app/src/main/java/aditishevale/com/instagramclone/Utils/SectionsStatePagerAdapter.java
+++ app/src/main/java/aditishevale/com/instagramclone/Utils/SectionsStatePagerAdapter.java
@@ -1,4 +1,30 @@
 package aditishevale.com.instagramclone.Utils;
 
-public class SectionsStatePagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final HashMap<Fragment,Integer> mFragmnets = new HashMap<>();
+    private final HashMap<String,Integer> mFragmnetNumbers = new HashMap<>();
+    private final HashMap<Integer,String> mFragmnetNames = new HashMap<>();
+
+    public SectionsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
