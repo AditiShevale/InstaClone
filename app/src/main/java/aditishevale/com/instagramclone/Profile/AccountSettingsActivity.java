8ea4912fed405ceaeaa5c9be20f82c37e6d252bb
aditishevale <aditi.shevale999@gmail.com>
2018-07-14 12:26:27 +0530

    EditProfileFragment Class created

--- app/src/main/java/aditishevale/com/instagramclone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/aditishevale/com/instagramclone/Profile/AccountSettingsActivity.java
@@ -53,7 +53,7 @@ public class AccountSettingsActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            setViewPager(position);
+//            setViewPager(position);
             }
         });
 
