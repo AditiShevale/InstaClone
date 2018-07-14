2c4990e03c5b9e169a98992edef28a0579527107
aditishevale <aditi.shevale999@gmail.com>
2018-07-14 22:58:07 +0530

    SignOutFragment Fragment Added

--- app/src/main/java/aditishevale/com/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/aditishevale/com/instagramclone/Profile/EditProfileFragment.java
@@ -16,7 +16,7 @@ public class EditProfileFragment extends Fragment {
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_editprofile,container,false);
         Log.d("this","EditProfile Fragment");
         return view;
 }
