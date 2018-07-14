a8f27454106fa7153681733d47b43764811c5e3e
aditishevale <aditi.shevale999@gmail.com>
2018-07-14 20:16:43 +0530

    EditProfileFragment Class created

--- app/src/main/java/aditishevale/com/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/aditishevale/com/instagramclone/Profile/EditProfileFragment.java
@@ -1,4 +1,21 @@
 package aditishevale.com.instagramclone.Profile;
 
-public class EditProfileFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import aditishevale.com.instagramclone.R;
+
+public class EditProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        return view;
+}
 }
