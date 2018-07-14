b67964fb2b00be61898b4a14fa405a55f0f2b3fd
aditishevale <aditi.shevale999@gmail.com>
2018-07-14 22:55:34 +0530

    SignOutFragment Class Toast Added

--- app/src/main/java/aditishevale/com/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/aditishevale/com/instagramclone/Profile/EditProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,7 @@ public class EditProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
-
+        Log.d("this","EditProfile Fragment");
         return view;
 }
 }
