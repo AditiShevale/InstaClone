2f05a507fb375f079fd5efc6213c6907142758b3
aditishevale <aditi.shevale999@gmail.com>
2018-07-14 20:18:04 +0530

    SignOutFragment Class created

---
+++ app/src/main/java/aditishevale/com/instagramclone/Profile/SignOutFragment.java
@@ -0,0 +1,22 @@
+package aditishevale.com.instagramclone.Profile;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import aditishevale.com.instagramclone.R;
+
+public class SignOutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        return view;
+}
+}
