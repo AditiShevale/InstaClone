8214b283b9ca659041475e3aad8ce19d455e2099
aditishevale <aditi.shevale999@gmail.com>
2018-07-26 15:06:33 +0530

    Updated

--- app/src/main/java/aditishevale/com/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/aditishevale/com/instagramclone/Home/HomeActivity.java
@@ -45,6 +45,8 @@ public class HomeActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+
+                /*Icons*/
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
